4ccd0654ed00ec0cb11c434adcc40a1aed2d9ca9
Joseph Gallo <jmgallo55@gmail.com>
2017-06-27 14:49:14 -0700

    Initial Page
    Initial Page

---
+++ main.js
@@ -0,0 +1,17 @@
+function alternate(id){ 
+ if(document.getElementsByTagName){  
+    var table = document.getElementById(id);   
+    var rows = table.getElementsByTagName("tr");   
+    for(i = 0; i < rows.length; i++){           
+  //manipulate rows 
+      if(i % 2 == 0){ 
+        rows[i].className = "even";		
+      }else{ 
+        rows[i].className = "odd"; 
+      }       
+    } 
+  } 
+}
+
+/*  function code from:
+https://www.sitepoint.com/background-colors-javascript/ */
